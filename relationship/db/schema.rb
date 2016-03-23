db9fc8bfd1a0a81a5afa6ed95e6145decea0c807
Hernan Y.Ke <yuriqiao@gmail.com>
2016-03-23 14:10:24 -0400

    rails db:migrate

--- relationship/db/schema.rb
+++ relationship/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323172448) do
+ActiveRecord::Schema.define(version: 20160323180817) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -41,5 +41,20 @@ ActiveRecord::Schema.define(version: 20160323172448) do
   end
 
   add_index "contacts", ["company_id"], name: "index_contacts_on_company_id"
+
+  create_table "contacts_projects", force: :cascade do |t|
+    t.integer "contact_id"
+    t.integer "project_id"
+  end
+
+  add_index "contacts_projects", ["contact_id"], name: "index_contacts_projects_on_contact_id"
+  add_index "contacts_projects", ["project_id"], name: "index_contacts_projects_on_project_id"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "status",     default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
