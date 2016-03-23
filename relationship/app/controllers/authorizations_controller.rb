13881868e6ed89f1bbe10e4dc46129b2bda3fa52
Hernan Y.Ke <yuriqiao@gmail.com>
2016-03-23 17:12:05 -0400

    rails end

--- relationship/app/controllers/authorizations_controller.rb
+++ relationship/app/controllers/authorizations_controller.rb
@@ -1,8 +1,8 @@
 class AuthorizationsController < ActionController::API
 	def create
 		auth_client = Google::APIClient::ClientSecrets.new('web'=>{
-			client_id:ENV[''],
-			client_secret:ENV[''],
+			client_id:'71354611982-e0tbmt9j3eh3mo0pld3p21bd7n0a7qog.apps.googleusercontent.com',
+			client_secret:'KxlR-0lssO6nhShSOsj1YkCg',
 			redirect_url:'http://localhost:4200/oauth2callback'
 			}).to_authoration
 		client.grant_type = 'authorization_code'
