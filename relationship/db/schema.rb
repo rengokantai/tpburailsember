40bced59e3e021627b630c1e902bd7d2bef09a42
Hernan Y.Ke <yuriqiao@gmail.com>
2016-03-23 14:12:48 -0400

    add task

--- relationship/db/schema.rb
+++ relationship/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323180817) do
+ActiveRecord::Schema.define(version: 20160323181151) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -56,5 +56,16 @@ ActiveRecord::Schema.define(version: 20160323180817) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "status",      default: 0
+    t.integer  "project_id"
+    t.datetime "due_at"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
 
 end
